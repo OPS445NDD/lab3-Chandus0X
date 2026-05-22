e5db3c996b857f68bf0bf0df2a5d55ccdab150d0
bchand1 <bchand1@myseneca.ca>
2026-05-22 18:50:42 -0400

    Added lab3b

---
+++ lab3b.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Lab 3 part 1 script - functions '''
+#Author ID: bchand1
+
+def sum_numbers(number1, number2):
+    return int(number1) + int(number2)
+def subtract_numbers(number1, number2):
+    return int(number1) - int(number2)
+def multiply_numbers(number1, number2):
+    return int(number1) * int(number2)
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(sum_numbers(10,5))
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))
+
+import lab3b
+lab3b.sum_numbers(5, 10)
