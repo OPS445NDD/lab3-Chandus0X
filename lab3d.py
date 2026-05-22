25cc96cf1cb35c77581b1b9e97c9d419dbf72e83
bchand1 <bchand1@myseneca.ca>
2026-05-22 18:51:23 -0400

    Added lab3c

---
+++ lab3d.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#Author ID: bchand1
+
+
+
+import subprocess
+
+def free_space():
+    p = subprocess.Popen (
+            "df -h | grep '/$' | awk '{print $4}'",
+            stdout=subprocess.PIPE,
+           # stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True )
+    output = p.communicate()
+    stdout = output[0].decode('utf-8').strip()
+    return stdout
+print(free_space())
+
+
+
+
+ 
+
+
